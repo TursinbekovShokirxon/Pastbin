ff0cb0ca35f16c760a112b568a88cf9e8cd5300b
abdurasulov2203 <abdurasulov2203@gmail.com>
2024-07-11 19:21:07 +0500

    pagination

---
+++ Pastbin.Application/Pagination/PaginatedList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pastbin.Application.Pagination
+{
+    public class PaginatedList<T>:List<T>
+    {
+        public PaginatedList(IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            PageSize = pageSize;
+            TotalCount = source.Count();
+            TotalPages = (int)Math.Ceiling(TotalCount / (double)PageSize);
+
+            this.AddRange(source.Skip(PageIndex * PageSize).Take(PageSize));
+        }
+
+        public int PageIndex { get; private set; }
+        public int PageSize { get; private set; }
+        public int TotalCount { get; private set; }
+        public int TotalPages { get; private set; }
+
+        public bool HasPreviousPage
+        {
+            get
+            {
+                return (PageIndex > 0);
+            }
+        }
+
+        public bool HasNextPage
+        {
+            get
+            {
+                return (PageIndex + 1 < TotalPages);
+            }
+        }
+    }
+}
