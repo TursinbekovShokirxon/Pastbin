ebaed8138b5c6b9536ef1f5f3f8b4bf72ead66db
abdurasulov2203 <abdurasulov2203@gmail.com>
2024-07-12 19:00:23 +0500

    ыы

--- Pastbin.Application/Interfaces/IFileService.cs
+++ Pastbin.Application/Interfaces/IFileService.cs
@@ -7,7 +7,7 @@ namespace Pastbin.Application.Interfaces
 {
     public interface IFileService
     {
-        public Task<UploadFileResponse> UploadFileAsync(string bucketName, Stream fileStream,string fileName, int? expireHour,string? prefix);
+        public Task<UploadFileResponse> UploadFileAsync(string bucketName, Stream fileStream, string fileName, int? expireHour, string? prefix);
         public Task<IEnumerable<S3ObjectDTO>> GetAllFilesAsync(string bucketName, string? prefix);
         public Task<Stream> GetFileByKeyAsync(string bucketName, string key);
         public Task<bool> DeleteFileAsync(string bucketName, string key);
