9f8b36852fc0944f6083b3bc20170adec971d967
abdurasulov2203 <abdurasulov2203@gmail.com>
2024-07-12 20:47:42 +0500

    s

--- Pastbin.Application/Interfaces/IPostService.cs
+++ Pastbin.Application/Interfaces/IPostService.cs
@@ -9,14 +9,10 @@ namespace Pastbin.Application.Interfaces
 {
     public interface IPostService:ICRUDService<Post>
     {
-<<<<<<< HEAD
        public Task<Post> CreateAsync(Post entity,string text);
         public Task<IEnumerable<Post>> GetAllAsync();
         public Task<Post> GetByIdAsync(int id);
         public Task<Post> UpdateAsync(Post entity);
         public Task<bool> DeleteAsync(int id);
-=======
-        Task<Post> CreateAsync(Post entity,string text);
->>>>>>> 910ea9b5db315ad83f318eda10a2e2701bd1a94d
     }
 }
