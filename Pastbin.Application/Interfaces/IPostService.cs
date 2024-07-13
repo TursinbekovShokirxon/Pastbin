df23cd4cc52c654d6a2778679ba1b17f3e519677
Shokir <tursinbekovshokirxon@gmail.com>
2024-07-13 10:36:42 +0500

    фыв

--- Pastbin.Application/Interfaces/IPostService.cs
+++ Pastbin.Application/Interfaces/IPostService.cs
@@ -7,14 +7,12 @@ using System.Threading.Tasks;
 
 namespace Pastbin.Application.Interfaces
 {
-    public interface IPostService
+    public interface IPostService:ICRUDService<Post>
     {
        public Task<Post> CreateAsync(Post entity,string text);
-        public Task<IEnumerable<Post>> GetAllFromUsernameAsync(string Username);
         public Task<IEnumerable<Post>> GetAllAsync();
         public Task<Post> GetByIdAsync(int id);
         public Task<Post> UpdateAsync(Post entity);
         public Task<bool> DeleteAsync(int id);
-
     }
 }
